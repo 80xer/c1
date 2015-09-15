7ee03486ce52738f29efddeb4c85ce185cb6d1cb
de.lee <de.lee@lawcompany.co.kr>
2015-09-15 21:00:37 +0900

    add timer and split function

--- main.js
+++ main.js
@@ -20,6 +20,20 @@ $(function() {
 
   initMenu();
 
+  var sc = new Date();
+  var $time = $('#time');
+
+  var reader;
+  var progress = document.querySelector('.percent');
+
+  function updateTime() {
+    $time.text(((new Date()) - sc)/1000);
+    setTimeout(updateTime, 1000);
+  }
+
+  updateTime();
+
+
   $('#calcBtn').click(function (e) {
     if ( !($('#selectFile').val() && $('#sYear').val() && $('#sTerm').val()) ) {
       $('#calcAlertWrap').removeClass('mh0');
@@ -27,41 +41,80 @@ $(function() {
         $('#calcAlertWrap').addClass('mh0');
       }, 1000);
     }
-  })
+  });
 
   $("#selectBtn").change(function (e) {
     $("#selectFile").val($(this).val());
     if ($(this).val()) {
       $('#loadingWrap').removeClass('mh0');
       setTimeout(function(){
-        handleFile(e);
+        handleFileSelect(e);
       }, 250);
     }
   });
 
-  function handleFile(e) {
-    $('#loadingWrap').removeClass('mh0');
-    var files = e.target.files;
-    var i,f;
-    for (i = 0, f = files[i]; i != files.length; ++i) {
-      var reader = new FileReader();
-      var name = f.name;
-      reader.onload = function(e) {
-        setTimeout(function() {
-          $('#loadingWrap').addClass('mh0');
-        }, 500);
-        var data = e.target.result;
+  function abortRead() {
+    reader.abort();
+  }
 
-        var workbook = XLSX.read(data, {type: 'binary'});
-        
-        /* Get worksheet */
-        var wsInput = workbook.Sheets['Input_보유'];
+  function errorHandler(evt) {
+    switch(evt.target.error.code) {
+      case evt.target.error.NOT_FOUND_ERR:
+        alert('파일이 없습니다.');
+        break;
+      case evt.target.error.NOT_READABLE_ERR:
+        alert('파일을 읽을 수 없습니다.');
+        break;
+      case evt.target.error.ABORT_ERR:
+        break; // noop
+      default:
+        alert('파일을 읽는 도중 에러가 발생했습니다.');
+    };
+  }
 
-        /* Find desired cell */
-        var wsInputA1 = wsInput['A1'];
-
-      };
-      reader.readAsBinaryString(f);
+  function updateProgress(evt) {
+    // evt is an ProgressEvent.
+    if (evt.lengthComputable) {
+      var percentLoaded = Math.round((evt.loaded / evt.total) * 100);
+      // Increase the progress bar length.
+      if (percentLoaded < 100) {
+        progress.style.width = percentLoaded + '%';
+        progress.textContent = percentLoaded + '%';
+      }
     }
+  }
+
+  function handleFileSelect(evt) {
+    // Reset progress indicator on new file selection.
+    progress.style.width = '0%';
+    progress.textContent = '0%';
+
+    reader = new FileReader();
+    reader.onerror = errorHandler;
+    reader.onprogress = updateProgress;
+    reader.onabort = function(e) {
+      alert('File read cancelled');
+    };
+    reader.onloadstart = function(e) {
+      document.getElementById('progress_bar').className = 'loading';
+    };
+    reader.onload = function(e) {
+      $('#loadingWrap').addClass('mh0');
+
+      calc(e.target.result);
+      // Ensure that the progress bar displays 100% at the end.
+      progress.style.width = '100%';
+      progress.textContent = '100%';
+      setTimeout("document.getElementById('progress_bar').className='';", 2000);
+    }
+
+    // Read in the image file as a binary string.
+    reader.readAsBinaryString(evt.target.files[0]);
+  }
+
+  function calc(data) {
+    var workbook = XLSX.read(data, {type: 'binary'});
+    var wsInput = workbook.Sheets['Input_보유'];
+    var wsInputA1 = wsInput['A1'];
   }
 });
